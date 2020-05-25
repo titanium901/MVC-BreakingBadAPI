90dfb1db3b926208856144e14c345e75d761cf5a
titanium901 <nemes901@hotmail.com>
2020-05-25 18:39:58 +0300

    for test

---
+++ MVC-BreakingBadAPI/Model/TextChecker.swift
@@ -0,0 +1,37 @@
+//
+//  TextChecker.swift
+//  MVC-BreakingBadAPI
+//
+//  Created by Iury Popov on 09.05.2020.
+//  Copyright © 2020 Iurii Popov. All rights reserved.
+//
+
+import Foundation
+
+struct TextChecker {
+    
+    var text: String
+    var isValid: Bool!
+    static var searchValidText: String!
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+    mutating func checkUserInput(){
+        if text.isEmpty {
+            isValid = false
+        } else {
+            isValid = true
+            TextChecker.searchValidText = makeStringForSearchRequest(text)
+        }
+    }
+    
+    private func makeStringForSearchRequest(_ text: String) -> String {
+        return text.trimmingCharacters(in:
+            .whitespacesAndNewlines)
+            .replacingOccurrences(of: " ", with: "+")
+            .lowercased()
+            .capitalized
+    }
+}
